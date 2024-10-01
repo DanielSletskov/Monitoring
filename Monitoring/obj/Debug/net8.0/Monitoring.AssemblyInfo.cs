367615bcb562b538c726e92feabc8c27c3e153b0
Daniel <danielsletskov@gmail.com>
2024-10-01 15:02:48 +0200

    test

--- Monitoring/obj/Debug/net8.0/Monitoring.AssemblyInfo.cs
+++ Monitoring/obj/Debug/net8.0/Monitoring.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76831030d2fd0516ed8ed0bffe9ed72f7e3ebe6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6585f73772e901c3dcd09c7792670a8034919f75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
